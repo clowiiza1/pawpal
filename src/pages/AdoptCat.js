d1bb920b0901f0919ac52b627ddc0bb098db01a2
clowiiiza <43036376@mynwu.ac.za>
2024-08-24 10:26:51 +0200

    trying to fix server issues

--- src/pages/AdoptCat.js
+++ src/pages/AdoptCat.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import AnimalTable from '../components/AnimalTable';
 
 const AdoptCat = () => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className=" bg-pr text-center">
+    <div className="p-8 bg-pr text-center">
       <h1 className="text-4xl font-bold mb-4">Welcome to PawPal</h1>
       <p className="text-lg mb-8">Find a Friend, Make a Difference.</p>
-      <Link to="/animals" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+      {/* <Link to="/animals" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
         View Animals
-      </Link>
+      </Link> */}
       <Link to="/volunteer" className="ml-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
         Volunteer
       </Link>
